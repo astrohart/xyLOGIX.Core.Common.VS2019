0ca052ec548ad24597e1dd40d69d27940ac8223a
Brian Hart <brian.hart@xylogix.net>
2024-07-19 19:00:27 -0600

    Remove NULs and fix encodings

--- xyLOGIX.Core.Common.Interfaces/ISystem.cs
+++ xyLOGIX.Core.Common.Interfaces/ISystem.cs
@@ -1,4 +1,4 @@
-﻿namespace xyLOGIX.Core.Common.Interfaces
+namespace xyLOGIX.Core.Common.Interfaces
 {
     /// <summary>
     /// Defines the publicly-exposed methods and properties of system-command
--- xyLOGIX.Core.Common/Do.cs
+++ xyLOGIX.Core.Common/Do.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading;
 using xyLOGIX.Core.Common.Params.Factories;
 using xyLOGIX.Core.Common.Params.Interfaces;
