4b40e4f9c765e048e3002fc599ed5a143e303a8e
Brian Hart <brian.hart@xylogix.net>
2025-07-20 13:24:01 -0600

    Update Run.cs
    - Improve logging and error handling for child process execution
    - Enhance debug message categorization with logging levels
    - Introduce DetermineCurrentWorkingDirectory method for clarity
    - Add error handling for process start failures
    - Change result return type from read-only collection to array
    - Include logging for success or failure of command output

--- xyLOGIX.Core.Common/Run.cs
+++ xyLOGIX.Core.Common/Run.cs
@@ -129,10 +129,9 @@ namespace xyLOGIX.Core.Common
                     return result;
                 }
 
-                // Dump the argument of the parameter, command, to the log
                 DebugUtils.WriteLine(
-                    DebugLevel.Debug,
-                    $@"{workingDirectory.RemoveTrailingBackslashes() ?? Directory.GetCurrentDirectory().RemoveTrailingBackslashes()}\> {command}"
+                    DebugLevel.Info,
+                    "*** FYI *** Allocating a new buffer to hold the output of the child process..."
                 );
 
                 var buffer = new List<string>();
@@ -143,10 +142,7 @@ namespace xyLOGIX.Core.Common
                         Environment.ExpandEnvironmentVariables("%COMSPEC%");
                     proc.StartInfo.Arguments = $"/C {command}";
                     proc.StartInfo.WorkingDirectory =
-                        string.IsNullOrWhiteSpace(workingDirectory) ||
-                        !Directory.Exists(workingDirectory)
-                            ? Directory.GetCurrentDirectory()
-                            : workingDirectory;
+                        DetermineCurrentWorkingDirectory(workingDirectory);
 
                     proc.StartInfo.UseShellExecute = false;
                     proc.StartInfo.CreateNoWindow = true;
@@ -162,7 +158,7 @@ namespace xyLOGIX.Core.Common
                                 buffer.Add(e.Data);
                             }
 
-                        DebugUtils.WriteLine(e.Data);
+                        DebugUtils.WriteLine(DebugLevel.Debug, e.Data);
                     };
                     proc.ErrorDataReceived += (s, e) =>
                     {
@@ -172,29 +168,183 @@ namespace xyLOGIX.Core.Common
                             buffer.Add(e.Data);
                         }
 
-                        DebugUtils.WriteLine(e.Data);
+                        DebugUtils.WriteLine(DebugLevel.Debug, e.Data);
                     };
 
                     DebugUtils.WriteLine(
                         DebugLevel.Info,
-                        $"*** FYI *** Executing: {proc.StartInfo.FileName} {proc.StartInfo.Arguments}"
+                        "*** FYI *** Executing the specified command..."
                     );
 
-                    if (!proc.Start()) return result;
+                    DebugUtils.WriteLine(
+                        DebugLevel.Debug,
+                        $@"{DetermineCurrentWorkingDirectory(workingDirectory)}\> {command}"
+                    );
+
+                    DebugUtils.WriteLine(
+                        DebugLevel.Info,
+                        "Run.CommandWithOutput: Attempting to spawn the child process..."
+                    );
+
+                    // Check whether a new process was started.  If this is not the case, then
+                    // write an error message to the log file, and then terminate the execution
+                    //  of this method, returning the default return value, which is the empty
+                    // collection of strings.
+                    if (!proc.Start())
+                    {
+                        // No new process was started.  This is not desirable.
+                        DebugUtils.WriteLine(
+                            DebugLevel.Error,
+                            "Run.CommandWithOutput: No new process was started. Stopping..."
+                        );
+
+                        DebugUtils.WriteLine(
+                            DebugLevel.Debug,
+                            $"Run.CommandWithOutput: Result = '{result.ToSetString()}'"
+                        );
+
+                        return result;
+                    }
+
+                    DebugUtils.WriteLine(
+                        DebugLevel.Info,
+                        "Run.CommandWithOutput: *** SUCCESS *** The new process was started successfully.  Proceeding..."
+                    );
 
                     proc.BeginOutputReadLine();
                     proc.BeginErrorReadLine();
                     proc.WaitForExit();
                 }
 
-                result = buffer.AsReadOnly();
+                result = buffer.ToArray();
             }
             catch (Exception ex)
             {
                 DebugUtils.LogException(ex);
-                result = Enumerable.Empty<string>()
-                                   .ToList();
+
+                result = Array.Empty<string>();
             }
+
+            DebugUtils.WriteLine(
+                result.Count > 0 ? DebugLevel.Info : DebugLevel.Error,
+                result.Count > 0
+                    ? $"*** SUCCESS *** {result.Count} line(s) of STDOUT and STDERR output were obtained from spawning the child process.  Proceeding..."
+                    : "*** ERROR *** Zero line(s) of output were obtained from spawning the child process.  Stopping..."
+            );
+
+
+            return result;
+        }
+
+        /// <summary>
+        /// Determines the current working directory, optionally using a specified
+        /// <paramref name="folder" />.
+        /// </summary>
+        /// <remarks>
+        /// This method logs informational and error messages during its
+        /// execution.
+        /// <para />
+        /// If an exception occurs, it logs the exception details and defaults to returning
+        /// the current directory.
+        /// </remarks>
+        /// <param name="folder">
+        /// (Optional.) A <see cref="T:System.String" /> containing
+        /// the fully-qualified pathname of a folder to use as the working directory for
+        /// spawning a process.
+        /// <para />
+        /// If this parameter is <see langword="null" />, blank, or the
+        /// <see cref="F:System.String.Empty" /> value, the method returns the current
+        /// directory of the application.
+        /// </param>
+        /// <returns>
+        /// The effective working directory path, with trailing backslashes
+        /// removed. If <paramref name="folder" /> is null or empty, the current directory
+        /// of the application is returned.
+        /// </returns>
+        private static string DetermineCurrentWorkingDirectory(
+            [NotLogged] string folder = ""
+        )
+        {
+            var result = Directory.GetCurrentDirectory()
+                                  .RemoveTrailingBackslashes();
+
+            try
+            {
+                DebugUtils.WriteLine(
+                    DebugLevel.Info,
+                    "Run.DetermineCurrentWorkingDirectory *** INFO: Checking whether the value of the parameter, 'folder', is blank..."
+                );
+
+                // Check whether the value of the parameter, 'folder', is blank.
+                // If this is so, then emit an error message to the log file, and
+                // then terminate the execution of this method.
+                if (string.IsNullOrWhiteSpace(folder))
+                {
+                    // The parameter, 'folder' was either passed a null value, or it is blank.  This is not desirable.
+                    DebugUtils.WriteLine(
+                        DebugLevel.Error,
+                        "Run.DetermineCurrentWorkingDirectory: The parameter, 'folder', was either passed a null value, or it is blank. Stopping..."
+                    );
+
+                    DebugUtils.WriteLine(
+                        DebugLevel.Debug,
+                        $"Run.DetermineCurrentWorkingDirectory: Result = '{result}'"
+                    );
+
+                    // stop.
+                    return result;
+                }
+
+                DebugUtils.WriteLine(
+                    DebugLevel.Info,
+                    "*** SUCCESS *** The parameter 'folder', is not blank.  Proceeding..."
+                );
+
+                DebugUtils.WriteLine(
+                    DebugLevel.Info,
+                    $"Run.DetermineCurrentWorkingDirectory *** INFO: Checking whether the folder with path, '{folder}', exists on the file system..."
+                );
+
+                // Check whether a folder having the path, 'folder', exists on the file system.
+                // If it does not, then write an error message to the log file, and then terminate
+                // the execution of this method, returning the default return value.
+                if (!Directory.Exists(folder))
+                {
+                    DebugUtils.WriteLine(
+                        DebugLevel.Error,
+                        $"Run.DetermineCurrentWorkingDirectory: *** ERROR *** The system could not locate the folder having the path, '{folder}', on the file system.  Stopping..."
+                    );
+
+                    DebugUtils.WriteLine(
+                        DebugLevel.Debug,
+                        $"*** Run.DetermineCurrentWorkingDirectory: Result = '{result}'"
+                    );
+
+                    // stop.
+                    return result;
+                }
+
+                DebugUtils.WriteLine(
+                    DebugLevel.Info,
+                    $"Run.DetermineCurrentWorkingDirectory: *** SUCCESS *** The folder with path, '{folder}', was found on the file system.  Proceeding..."
+                );
+
+                result = folder.RemoveTrailingBackslashes();
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+
+
+                result = Directory.GetCurrentDirectory()
+                                  .RemoveTrailingBackslashes();
+            }
+
+            DebugUtils.WriteLine(
+                DebugLevel.Debug,
+                $"Run.DetermineCurrentWorkingDirectory: Result = '{result}'"
+            );
 
             return result;
         }
