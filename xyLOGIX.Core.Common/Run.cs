5c4f013f40a3e54e66d8d73585598328f7b2cb45
Brian Hart <brian.hart@xylogix.net>
2025-07-20 13:28:16 -0600

    Update Run.cs
    - Modify the constructor of the `Run` class to be protected and empty, preventing direct instantiation.
    - Remove the previous constructor implementation.
    - Add comments to clarify the purpose of the `System` property as a singleton instance.
    - Improve code readability in `OutputDataReceived` and `ErrorDataReceived` handlers by adjusting brace placement.

--- xyLOGIX.Core.Common/Run.cs
+++ xyLOGIX.Core.Common/Run.cs
@@ -22,19 +22,18 @@ namespace xyLOGIX.Core.Common
 
         /// Empty, protected constructor to prohibit direct allocation of this class.
         [Log(AttributeExclude = true)]
-        protected Run()
-        { }
-
-        /// Gets a reference to the one and only instance of
-        /// <see cref="T:xyLOGIX.Core.Common.Run" />
-        /// .
-        public static ISystem System { [DebuggerStepThrough] get; } = new Run();
+        protected Run() { }
 
         /// <summary>
         /// Gets a reference to an instance of an object that is to be used for thread
         /// synchronization purposes.
         /// </summary>
         private static object SyncRoot { get; } = new object();
+
+        /// Gets a reference to the one and only instance of
+        /// <see cref="T:xyLOGIX.Core.Common.Run" />
+        /// .
+        public static ISystem System { [DebuggerStepThrough] get; } = new Run();
 
         /// Runs the specified system
         /// <paramref name="command" />
@@ -153,17 +152,17 @@ namespace xyLOGIX.Core.Common
 
                     proc.OutputDataReceived += (s, e) =>
                     {
-                        if (e.Data == null) return;       // end-of-stream
-                            lock (SyncRoot)
-                            {
-                                buffer.Add(e.Data);
-                            }
+                        if (e.Data == null) return; // end-of-stream
+                        lock (SyncRoot)
+                        {
+                            buffer.Add(e.Data);
+                        }
 
                         DebugUtils.WriteLine(DebugLevel.Debug, e.Data);
                     };
                     proc.ErrorDataReceived += (s, e) =>
                     {
-                        if (e.Data == null) return;        // end-of-stream
+                        if (e.Data == null) return; // end-of-stream
                         lock (SyncRoot)
                         {
                             buffer.Add(e.Data);
